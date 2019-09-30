4349e7e1ab08c2928c6c9f4b21d4a97d51df2cd2
kbetlach <kbetlach@gmail.com>
2019-09-30 18:30:52 -0500

    array creation, starting conditionals

--- homework03/assets/script.js
+++ homework03/assets/script.js
@@ -1,22 +1,34 @@
-var special = "!#$%&'()*+,-./:;<=>?@[\]^_'"
-var numeric = "1234567890"
-var lowerCase = "abcdefghijklmnopqrstuvwxyz"
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+//make an array of these
 
-var characters = prompt("How many characters would you like your password to contain?")
-var special = confirm("Click OK to confirm including special characters in your password.")
-var number = confirm("Click OK to confirm including numeric characters in your password.")
-var lowerCase = confirm("Click OK to confirm including lower case letters in your password.")
-var upperCase = confirm("Click OK to confirm including upper case letters in your password.")
+var userChoice = ["special", "numeric", "lowerCase", "upperCase"];
 
+var special = "!#$%&'()*+,-./:;<=>?@[\]^_'";
+var numeric = "1234567890";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-function generate (){
-    var password = '';
+var characters = parseInt(prompt("How many characters would you like your password to contain?"));
+//create if else for character counts
 
-    for(var i = 0; i < characters.length; i++){
-        password += characters.charAt(math.floor(Math.random() * characters.length));
+    if (userChoice < 128) {
+        alert("You must pick a character count equal to or less than 128 characters!");
     }
 
-    document.getElementById("placeholder").value = password;
+//create conditionals for which options are chosen
+//look into sort method
+var special = confirm("Click OK to confirm including special characters in your password.");
+var number = confirm("Click OK to confirm including numeric characters in your password.");
+var lowerCase = confirm("Click OK to confirm including lower case letters in your password.");
+var upperCase = confirm("Click OK to confirm including upper case letters in your password.");
 
+
+//parseInt
+function generate (){
+    var password = '';
+//switch from = to into populating an array
+    for(var i = 0; i < characters; i++){
+        password = characters.charAt(math.floor(Math.random() * characters.length));
+    }
 }
+
+//display result to user as an alert
