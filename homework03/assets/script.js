b81e25099e0c96b742965faa803e73a4b78d60ef
kbetlach <kbetlach@gmail.com>
2019-10-02 18:31:58 -0500

    reworked generate function. also Blake is a king

--- homework03/assets/script.js
+++ homework03/assets/script.js
@@ -1,12 +1,21 @@
-var userChoice = ""
+//redo array
+
+var userChoice = "";
 
 var special = "!#$%&'()*+,-./:;<=>?@[\]^_'";
 var numeric = "1234567890";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-var characters = parseInt(prompt("How many characters would you like your password to contain?"));
-//create if else for character counts
+
+
+
+//separate out (cleaner)
+ 
+
+    function generate (){
+        userChoice = "";
+        var characters = parseInt(prompt("How many characters would you like your password to contain?"));
 
     if (characters > 128) {
         alert("You must pick a character count equal to or less than 128 characters!"); 
@@ -15,7 +24,6 @@ var characters = parseInt(prompt("How many characters would you like your passwo
         alert("You must pick a character count equal to or greater than 8 characters!")
     }
     
-//create conditionals for which options are chosen
 //look into sort method
 
 var specialChoice = confirm("Click OK to confirm including special characters in your password.");
@@ -23,17 +31,25 @@ var numberChoice = confirm("Click OK to confirm including numeric characters in 
 var lowerCaseChoice = confirm("Click OK to confirm including lower case letters in your password.");
 var upperCaseChoice = confirm("Click OK to confirm including upper case letters in your password.");
 
-    if(specialChoice) userChoice += special; 
-    if(numberChoice)  userChoice += number;
-    if(lowerCaseChoice)  userChoice += lowerCase;
-    if(upperCaseChoice)  userChoice += upperCase; {
-        function generate (){
-            var password = '';
-            for(var i = 0; i < userChoice; i++){
-                password += userChoice.charAt(math.floor(Math.random() * characters.length));
-            }
+if(specialChoice){
+    userChoice += special; 
+}
+if(numberChoice){
+     userChoice += numeric;
+}
+if(lowerCaseChoice)  userChoice += lowerCase;
+if(upperCaseChoice)  userChoice += upperCase; 
+
+        var password = '';
+        console.log(userChoice)
+        for(var i = 0; i < characters; i++){
+           var password1 = userChoice.charAt(Math.floor(Math.random() * userChoice.length));
+           password = password + password1;
+
         }
+        document.getElementById("field").innerHTML = password;
     }
-    console.log(generate);
+    var buttonClick = document.getElementById("generate")
+    buttonClick.addEventListener("click", generate);
 
 //display result to user as an alert
