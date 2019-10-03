4021f6e59f173170ca4e5959c40678aa40dcd1af
kbetlach <kbetlach@gmail.com>
2019-10-03 15:14:16 -0500

    javascript code cleanup. added return false to conditionals

--- homework03/assets/script.js
+++ homework03/assets/script.js
@@ -1,5 +1,3 @@
-//redo array
-
 var userChoice = "";
 
 var special = "!#$%&'()*+,-./:;<=>?@[\]^_'";
@@ -7,24 +5,18 @@ var numeric = "1234567890";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-
-
-
-//separate out (cleaner)
- 
-
     function generate (){
         userChoice = "";
         var characters = parseInt(prompt("How many characters would you like your password to contain?"));
 
     if (characters > 128) {
         alert("You must pick a character count equal to or less than 128 characters!"); 
+        return false;
     }
     if (characters < 8) {
         alert("You must pick a character count equal to or greater than 8 characters!")
+        return false;
     }
-    
-//look into sort method
 
 var specialChoice = confirm("Click OK to confirm including special characters in your password.");
 var numberChoice = confirm("Click OK to confirm including numeric characters in your password.");
@@ -37,19 +29,19 @@ if(specialChoice){
 if(numberChoice){
      userChoice += numeric;
 }
-if(lowerCaseChoice)  userChoice += lowerCase;
-if(upperCaseChoice)  userChoice += upperCase; 
+if(lowerCaseChoice){
+    userChoice += lowerCase;
+}
+if(upperCaseChoice){
+    userChoice += upperCase; 
+}
 
         var password = '';
-        console.log(userChoice)
         for(var i = 0; i < characters; i++){
            var password1 = userChoice.charAt(Math.floor(Math.random() * userChoice.length));
            password = password + password1;
-
         }
         document.getElementById("field").innerHTML = password;
     }
     var buttonClick = document.getElementById("generate")
     buttonClick.addEventListener("click", generate);
-
-//display result to user as an alert
