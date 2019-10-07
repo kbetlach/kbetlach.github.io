665e2fca29b62892174d62fa1e84953874c514a9
kbetlach <kbetlach@gmail.com>
2019-10-07 15:57:04 -0500

    5 questions written in questions.js

---
+++ homework04/assets/questions.js
@@ -0,0 +1,52 @@
+var quizQuestions = [
+    {
+      question1: "What are the three main components that go into web design?",
+      answers: {
+        a: "JavaScript, JSON, jQuery",
+        b: "HTML, CSS, JavaScript",
+        c: "HTML, variables, Bootstrap",
+        d: "CSS, Terminal, Java"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question2: "What does MERN stand for in MERN full stack?",
+      answers: {
+        a: "ModularDB, Express, Reach, Node.js",
+        b: "MungoDB, Extracurricular, Rear, Null.js",
+        c: "MongoDB, Express, React, Node.js",
+        d: "MangoDB, Espresso, Reaction, NAN.js"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question3: "What is a true / false statement called in JavaScript?",
+      answers: {
+        a: "String",
+        b: "Function",
+        c: "Boolean",
+        d: "All of the above"
+      },
+      correctAnswer: "c"
+    },
+    {
+        question4: "What tool can be used to see if a JavaScript function is working or not?",
+        answers: {
+          a: "console.log",
+          b: "council.log",
+          c: "VS Code",
+          d: "W3 Schools"
+        },
+        correctAnswer: "a"
+      },
+      {
+        question5: "What is the name of the CSS framework developed by Twitter?",
+        answers: {
+          a: "Bootheel",
+          b: "Boolean",
+          c: "Bootlaces",
+          d: "Bootstrap"
+        },
+        correctAnswer: "d"
+      }
+  ];
