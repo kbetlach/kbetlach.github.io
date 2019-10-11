94e140f5e868fdf601c829bb55fd18300b2bfb5e
kbetlach <kbetlach@gmail.com>
2019-10-11 11:56:43 -0500

    function written to generate answer buttons, but not functioning yet

--- homework04/assets/questions.js
+++ homework04/assets/questions.js
@@ -1,52 +1,74 @@
-var questions = [
-    {
-      question: "What are the three main components that go into web design?",
-      answers: {
-        a: "JavaScript, JSON, jQuery",
-        b: "HTML, CSS, JavaScript",
-        c: "HTML, variables, Bootstrap",
-        d: "CSS, Terminal, Java"
-      },
-      correctAnswer: "b"
+var timeEl = document.querySelector(".time");
+var startEl = document.getElementById("start");
+var containerEl = document.querySelector(".container");
+var directionsEl = document.querySelector(".directions");
+var score = 0;
+var secondsLeft = 75;
+//global index for questions, increment by 1 to cycle through
+var questionNumber = 0;
+
+var myQuestions = [
+  {
+    question: "What are the three main components that go into web design?",
+    answers: ["JavaScript, JSON, jQuery", "HTML, CSS, JavaScript", "HTML, variables, Bootstrap","CSS, Terminal, Java"],
+    correctAnswer: "b"
+  },
+  {
+    question: "What does MERN stand for in MERN full stack?",
+    answers: ["ModularDB, Express, Reach, Node.js", "MungoDB, Extracurricular, Rear, Null.js", "MongoDB, Express, React, Node.js", "MangoDB, Espresso, Reaction, NAN.js"],
+    correctAnswer: "c"
+  },
+  {
+    question: "What is a true / false statement called in JavaScript?",
+    answers: ["String", "Function", "Boolean", "All of the above"],
+    correctAnswer: "c"
+  },
+  {
+      question: "What tool can be used to see if a JavaScript function is working or not?",
+      answers: ["console.log", "council.log", "VS Code", "W3 Schools"],
+      correctAnswer: "a"
     },
     {
-      question: "What does MERN stand for in MERN full stack?",
-      answers: {
-        a: "ModularDB, Express, Reach, Node.js",
-        b: "MungoDB, Extracurricular, Rear, Null.js",
-        c: "MongoDB, Express, React, Node.js",
-        d: "MangoDB, Espresso, Reaction, NAN.js"
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "What is a true / false statement called in JavaScript?",
-      answers: {
-        a: "String",
-        b: "Function",
-        c: "Boolean",
-        d: "All of the above"
-      },
-      correctAnswer: "c"
-    },
-    {
-        question: "What tool can be used to see if a JavaScript function is working or not?",
-        answers: {
-          a: "console.log",
-          b: "council.log",
-          c: "VS Code",
-          d: "W3 Schools"
-        },
-        correctAnswer: "a"
-      },
-      {
-        question: "What is the name of the CSS framework developed by Twitter?",
-        answers: {
-          a: "Bootheel",
-          b: "Boolean",
-          c: "Bootlaces",
-          d: "Bootstrap"
-        },
-        correctAnswer: "d"
-      }
-  ];
+      question: "What is the name of the CSS framework developed by Twitter?",
+      answers: ["Bootheel", "Boolean", "Bootlaces", "Bootstrap"],
+      correctAnswer: "d"
+    }
+];
+
+function startQuiz() {
+  event.preventDefault();
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left!";
+
+    directionsEl.style.display = "none";
+  
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      alert("BZZZZZZZZT! You're out of time!");
+    }
+
+  }, 1000);
+  
+  nextQuestion(questionNumber);
+}
+
+function nextQuestion(questionIndex) {
+  console.log(myQuestions[questionIndex].myQuestion)
+
+  quiz.innerHTML = myQuestions[0].question;
+  for (var i = 0; i < 4; i++){
+    console.log(i);
+  var button = $("<button>");
+  button.addClass("answers");
+  button.attr("data-answer",myQuestions[0].answers[i]);
+  button.text(myQuestions[0].answers[i]);
+  button.css("background-color", "yellow");
+  quiz.appendChild(button);
+
+  quiz.appendChild(quiz);
+  //feed parameter of current question
+}
+}
+
+//within next question function, write conditionals for right or wrong, include time penalties.
