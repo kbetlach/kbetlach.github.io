854c3db113a9595054d108798ce864eef90aa2d9
kbetlach <kbetlach@gmail.com>
2019-10-17 15:36:25 -0500

    timeblocks now display correct color. deleted moment.js file since it wasn't needed

--- homework05/assets/script.js
+++ homework05/assets/script.js
@@ -2,9 +2,14 @@ var today = moment().format('dddd, MMMM Do YYYY');
 $("#currentDay").text(today);
 console.log(today);
 
-var saveBtn = $(".saveBtn");
-
-$("save").on("click", function(store){
-    store.preventDefault;
-    //localStorage.setItem(//value and key?);
-});
+for (var i = 9; i < 18; i++) {
+    if (i < moment().hour()) {
+        $("#" + i).addClass("past")
+    }
+    else if (i === moment().hour()){
+        $("#" + i).addClass("present")
+    }
+    else {
+        $("#" + i).addClass("future")
+    }
+}
