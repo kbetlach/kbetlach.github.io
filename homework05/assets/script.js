6da4916a68efe6f83e082d7cccb4b41ca7525ea5
kbetlach <kbetlach@gmail.com>
2019-10-18 12:59:25 -0500

    giving buttons and textareas IDs. local storage saving edits

--- homework05/assets/script.js
+++ homework05/assets/script.js
@@ -13,3 +13,16 @@ for (var i = 9; i < 18; i++) {
         $("#" + i).addClass("future")
     }
 }
+
+$("button").on("click", function(event) {
+    event.preventDefault();
+    var btnValue = (this.id);
+    $("#textarea" + btnValue)
+    localStorage.setItem("plannerEvent", JSON.stringify(btnValue));
+    
+})
+
+$(document).ready(function(){
+    var savedText = JSON.parse(localStorage.getItem("plannerEvent"));
+    $("textarea").text = savedText;
+})
