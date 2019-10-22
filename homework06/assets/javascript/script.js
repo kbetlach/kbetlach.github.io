1d000b407d3cc23b5639c4d213035bc1027befdd
kbetlach <kbetlach@gmail.com>
2019-10-22 18:49:19 -0500

    more styling tweaks. added API key and query URL. started on click functions for generating city weather data

--- homework06/assets/javascript/script.js
+++ homework06/assets/javascript/script.js
@@ -1,2 +1,28 @@
-var APIKey
-var queryURL
+var citySelection = $("#city-search").val().trim();
+var apiKey = "b090192947ebb72ee8d89e585110389e"
+var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + citySelection +"&apikey=" + apiKey;
+
+function searchCity(cityName) {
+    
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+
+  console.log(queryURL);
+  console.log(response);
+
+    $(".city").html(response.name);
+    $(".temperature").html("Temperature: " + response.main.temp);
+    $(".humidity").html("Humidity: " + response.main.humidity);
+    $(".wind").html("Wind Speed: " + response.wind.speed);
+    $("uv").html("UV Index:" + response);
+  }
+)}
+
+$("#select-city").on("click", function(event) {
+    event.preventDefault();
+    var citySelection = $("#city-search").val().trim();
+
+    searchCity(citySelection);
+});
