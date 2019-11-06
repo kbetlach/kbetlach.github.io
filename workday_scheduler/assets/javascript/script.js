109eafc6e41250144580430ee6368d4547395adb
kbetlach <kbetlach@gmail.com>
2019-11-06 16:41:38 -0600

    timeblocks now save correctly. save button hover works as well

--- workday_scheduler/assets/javascript/script.js
+++ workday_scheduler/assets/javascript/script.js
@@ -20,24 +20,15 @@ for (var i = 9; i < 18; i++) {
 
 //Saving timeblock inputs into local storage, and reloading them on refresh.
 
-$("#button9").on("click", function(event) {
-  event.preventDefault();
-  var textInput9 = $("#textarea9").val();
-    localStorage.getItem("plannedEvent");
-   localStorage.setItem("plannedEvent", JSON.stringify(textInput9));
-   console.log('clicked',textInput9);
-})
-
-$("#button10").on("click", function(event) {
+$("button").on("click", function (event) {
     event.preventDefault();
-    var textInput10 = $("#textarea10").val();
-     localStorage.setItem("plannedEvent", JSON.stringify(textInput10));
-     console.log('clicked',textInput10);
-  })
+    var savedText = $("textarea#" + this.id).val();
+    localStorage.setItem("plannedEvent" + this.id, savedText)
+});
 
- $(document).ready(function(){
-    var savedText = JSON.parse(localStorage.getItem("plannedEvent"));
-     $("#textarea9").html(savedText);
-     $("#textarea10").html(savedText);
-     console.log(savedText);
- })
+$(document).ready(function () {
+    for (i = 9; i < 18; i++) {
+        var savedText = localStorage.getItem("plannedEvent" + i);
+        $("textarea#" + i).html(savedText);
+    }
+});
