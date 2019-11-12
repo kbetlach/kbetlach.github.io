cb7e15af20b8f59597600d856b2986ec3f11263a
kbetlach <kbetlach@gmail.com>
2019-11-12 16:43:58 -0600

    suser input now generates an html profile

--- pdf_generator/index.js
+++ pdf_generator/index.js
@@ -1,38 +1,66 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const util = require("util");
 const axios = require("axios");
 
-const writeFileAsync = util.promisify(fs.writeFile);
-
-inquirer
-  .prompt([
-    {
-    message: "What is your GitHub username?:",
-    name: "username",
-    },
-    {
-    message: "What is your favorite color?:",
-    name: "color"
-    },
+var promptUser = inquirer
+    .prompt([
+        {
+          type: "input",
+          message: "What is your name?",
+          name: "name",
+        },
+        {
+          type: "input",
+          message: "What is your GitHub username?:",
+          name: "github",
+        },
+        {
+          type: "input",
+          message: "What is your favorite color?:",
+          name: "color"
+        },
   ])
-  .then(function({ username }) {
-    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+  .then(function({ name, github, color }) {
+    const queryURL = `https://api.github.com/users/${github}`;
 
-    axios.get(queryUrl).then(function(response) {
-      const repoNames = response.data.map(function(repo) {
-        return repo.name;
-      });
+    axios.get(queryURL)
+      .then(function(response) {
+      fs.writeFile("profile.html",
 
-      const repoNamesStr = repoNames.join("\n");
+  `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>GitHub Profile Generator</title>
+  </head>
+  <body>
+    <div class="jumbotron jumbotron-fluid">
+    <div class="container" style="background-color: ${color}">
+      <h1 class="display-4">GitHub Snapshot for ${name}</h1>
+      <div><img src="${response.data.avatar_url}"></div>
+      <h2>${response.data.bio}</h2>
+      <h3><span class="badge badge-secondary">Contact Me</span></h3>
+      <ul class="list-group">
+        <li class="list-group-item">Location: ${response.data.location}</li>
+        <li class="list-group-item">My GitHub username is ${github}</li>
+        <li class="list-group-item">Public Repositories: ${response.data.public_repos}</li>
+        <li class="list-group-item">Followers: ${response.data.followers}</li>
+        <li class="list-group-item">Following: ${response.data.following}</li>
+        <li class="list-group-item">GitHub Stars: </li>
+      </ul>
+    </div>
+  </div>
+  </body>
+  </html>`,
 
-      fs.writeFile("profile.pdf", repoNamesStr, function(err) {
-        if (err) {
-          throw err;
-        }
+  (err) => {
+    if (err)
+        throw err;
 
-        console.log(`Saved ${repoNames.length} repos`);
-      });
-    });
-  });
+        console.log("Successfully created an html file!");      
 
+    })
+  })
+})
