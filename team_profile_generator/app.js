3af60f4b92858b1e10da3ed5a3e471edd10de6e7
kbetlach <kbetlach@gmail.com>
2019-11-21 14:36:40 -0600

    trimmed html. employee cards now append appropriately

--- team_profile_generator/app.js
+++ team_profile_generator/app.js
@@ -8,17 +8,22 @@ function generateTeamMember() {
             name: "teamMembers",
             choices: ["Engineer", "Intern", "I don't want to add any additional team members."]
         }]).then(function (answer) {
-            switch(answer.teamMembers) {
-                case "Engineer":
-                  generateEngineer();
-                  break;
-                case "Intern":
-                  generateIntern();
-                  break;
-                default:
-                  console.log("Your team is complete.");
-              }
-        })
+        switch (answer.teamMembers) {
+            case "Engineer": generateEngineer();
+                break;
+            case "Intern": generateIntern();
+                break;
+            default:
+                console.log("***** Your team is complete. *****");
+                fs.appendFile("./team.html", "</div></div></body></html>", (err) => {
+                    if (err) 
+                        throw err;
+                    
+
+
+                });
+        }
+    })
 }
 
 function generateManager() {
@@ -41,12 +46,9 @@ function generateManager() {
             message: "What is your manager's office number?",
             name: "managerOffice"
         }
-    ])
-    .then(function({ managerName, managerID, managerEmail, managerOffice }){
+    ]).then(function ({managerName, managerID, managerEmail, managerOffice}) {
         generateTeamMember();
-        fs.appendFile("./team.html",
-
-        `<!DOCTYPE html>
+        fs.writeFile("./team.html", `<!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
@@ -57,18 +59,7 @@ function generateManager() {
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         </head>
         <body>
-                <div class="card" style="width: 18rem;">
-                        <div class="card-body" style="background-color: gold;">
-                          <h5 class="card-title">${managerName}</h5>
-                          <p class="card-text"><i class="fas fa-coffee"></i> Manager</p>
-                        </div>
-                        <ul class="list-group list-group-flush">
-                          <li class="list-group-item">ID: ${managerID}</li>
-                          <li class="list-group-item">Email: ${managerEmail}</li>
-                          <li class="list-group-item">Office Number: ${managerOffice}</li>
-                        </ul>
-                </div>
-            
+
         <style>
         
         body {
@@ -96,14 +87,33 @@ function generateManager() {
         }
         
         </style>
-        
-        </body>
-        </html>`,
 
-(err) => {
-    if (err)
-        throw err;
-      }) 
+        <header>
+            <h1>A Snapshot of my Team</h1>
+        </header>
+
+ <br />
+ <div class="container">
+ <div class="row">
+ <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                        <div class="card-body" style="background-color: gold;">
+                          <h5 class="card-title">${managerName}</h5>
+                          <p class="card-text"><i class="fas fa-coffee"></i> Manager</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                          <li class="list-group-item">ID: ${managerID}</li>
+                          <li class="list-group-item">Email: ${managerEmail}</li>
+                          <li class="list-group-item">Office Number: ${managerOffice}</li>
+                        </ul>
+</div>
+                </div>`, (err) => {
+            if (err) 
+                throw err;
+            
+
+
+        })
     })
 }
 
@@ -126,22 +136,10 @@ function generateEngineer() {
             message: "What is your engineer's GitHub username?",
             name: "engineerGithub"
         }
-    ])
-    .then(function({ engineerName, engineerID, engineerEmail, engineerGithub }){
+    ]).then(function ({engineerName, engineerID, engineerEmail, engineerGithub}) {
         generateTeamMember();
-        fs.appendFile("./team.html",
-
-        `<!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>My Team - Profile</title>
-            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        </head>
-        <body>
+        fs.appendFile("./team.html", `
+        <div class="col-md-4">
                 <div class="card" style="width: 18rem;">
                         <div class="card-body" style="background-color: royalblue;">
                           <h5 class="card-title">${engineerName}</h5>
@@ -153,42 +151,13 @@ function generateEngineer() {
                           <li class="list-group-item">GitHub: ${engineerGithub}</li>
                         </ul>
                 </div>
+                </div>`, (err) => {
+            if (err) 
+                throw err;
             
-        <style>
-            
-        body {
-            font-family: 'Lora', serif;
-            background: url("images/interlaced.png");
-        }
 
-        header {
-            background-color: rosybrown;
-            text-align: center;
-            padding: 15px;
-        }
 
-        .card-body {
-            font-size: 24px;
-        }
-
-        .card-title {
-            font-size: 26px;
-        }
-
-        .card {
-            margin: 25px;
-            border: outset;
-        }
-        
-        </style>
-        
-        </body>
-        </html>`,
-
-(err) => {
-    if (err)
-        throw err;
-      }) 
+        })
     })
 }
 
@@ -211,22 +180,10 @@ function generateIntern() {
             message: "What is your intern's school?",
             name: "internSchool"
         }
-    ])
-    .then(function({ internName, internID, internEmail, internSchool }){
+    ]).then(function ({internName, internID, internEmail, internSchool}) {
         generateTeamMember();
-        fs.appendFile("./team.html",
-
-        `<!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>My Team - Profile</title>
-            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        </head>
-        <body>
+        fs.appendFile("./team.html", `
+            <div class="col-md-4">
                 <div class="card" style="width: 18rem;">
                         <div class="card-body" style="background-color: crimson;">
                           <h5 class="card-title">${internName}</h5>
@@ -238,42 +195,14 @@ function generateIntern() {
                           <li class="list-group-item">School: ${internSchool}</li>
                         </ul>
                 </div>
+            </div>
+                `, (err) => {
+            if (err) 
+                throw err;
             
-        <style>
-            
-        body {
-            font-family: 'Lora', serif;
-            background: url("images/interlaced.png");
-        }
 
-        header {
-            background-color: rosybrown;
-            text-align: center;
-            padding: 15px;
-        }
 
-        .card-body {
-            font-size: 24px;
-        }
-
-        .card-title {
-            font-size: 26px;
-        }
-
-        .card {
-            margin: 25px;
-            border: outset;
-        }
-        
-        </style>
-        
-        </body>
-        </html>`,
-
-(err) => {
-    if (err)
-        throw err;
-      }) 
+        })
     })
 }
 generateManager();
