12b9ca75adf88171c9045e6337e08bcdb62c13e5
kbetlach <kbetlach@gmail.com>
2019-12-13 13:30:05 -0600

    boilerplate code added, mysql schema and seeds created, image found, server works and talks to mysql

---
+++ da_burger/controllers/burger_controller.js
@@ -0,0 +1,15 @@
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burgers.js");
+
+router.get
+
+router.post
+
+router.put
+
+router.delete
+
+module.exports = router;
---
+++ da_burger/db/burger_seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Double Bacon Cheese");
+INSERT INTO burgers (burger_name) VALUES ("Impossible Burger with all the fixins'");
+INSERT INTO burgers (burger_name) VALUES ("Bunless Beyond Burger");
