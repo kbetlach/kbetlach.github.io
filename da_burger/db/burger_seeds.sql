9f155481c060a2e37ddd21f2a2f9a9af33075d35
kbetlach <kbetlach@gmail.com>
2019-12-20 11:56:29 -0600

    removed da_burger

--- da_burger/db/burger_seeds.sql
+++
@@ -1,5 +0,0 @@
-use burgers_db;
-
-INSERT INTO burgers (burger_name) VALUES ("Double Bacon Cheese");
-INSERT INTO burgers (burger_name) VALUES ("Impossible Burger with all the fixins'");
-INSERT INTO burgers (burger_name) VALUES ("Bunless Beyond Burger");
