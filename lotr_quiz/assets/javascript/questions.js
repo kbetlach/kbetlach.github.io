690492ef49da464d934de27c60fc82648927c447
kbetlach <kbetlach@gmail.com>
2019-12-23 15:02:27 -0600

    switching from a code quiz to a lotr quiz. questions changed

---
+++ lotr_quiz/assets/javascript/questions.js
@@ -0,0 +1,27 @@
+var myQuestions = [
+    {
+      question: "What is the name of the kingdom that Aragorn has a claim for?",
+      answers: ["The Shire", "Rohan", "Gondor", "Isengard"],
+      correctAnswer: "Gondor"
+    },
+    {
+      question: "What is the name of Frodo's sword?",
+      answers: ["Stink", "Sting", "Samwise", "Spider Slayer"],
+      correctAnswer: "Sting"
+    },
+    {
+      question: "Which animal is the land of Rohan known for?",
+      answers: ["Horses", "Goats", "Sheep", "Squirrels"],
+      correctAnswer: "Horses"
+    },
+    {
+      question: "What is the name of the wizard who wears a gray robe and hat?",
+      answers: ["Saruman", "Radagast", "Sauron", "Gandalf"],
+      correctAnswer: "Gandalf"
+    },
+    {
+      question: "Which people of Middle-Earth are immortal?",
+      answers: ["Hobbits", "Elves", "Dwarves", "Humans"],
+      correctAnswer: "Elves"
+    }
+  ];
